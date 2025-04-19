a1706534c56a05bc4777ce9ecf3dadf1f03b9a72
Kiran <121440995+KiranSHavalad@users.noreply.github.com>
2025-04-20 00:22:15 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from '../src/Components/Register'
 import Login from '../src/Components/login'
-import Home from './Components/home';
 function App() {
   return (
     <BrowserRouter>
@@ -10,11 +9,8 @@ function App() {
       <Route path="/"  exact element={<Login />} />
         <Route path="/login"  exact element={<Login />} />
         <Route path="/register" exact  element={<Register />} />
-        <Route path="/home" exact element={<Home/>}/>
       </Routes>
     </BrowserRouter>
-
   )
 }
-
 export default App
